47497da5625da4096a1895251f8d4efd81464c16
Kesisto <lassi.jk@gmail.com>
2015-10-09 14:22:44 +0300

    dfsf

--- KamkByte3/KamkByte3/Code/Window/WindowCreation.cpp
+++ KamkByte3/KamkByte3/Code/Window/WindowCreation.cpp
@@ -1,5 +1,7 @@
 #include "WindowCreation.h"
 #define PROGRAM_NAME "KamkByte3"
+static const int width = 1024;
+static const int height = 768;
 int WindowCreation::makeNewWindow(int argc, char* argv[])
 {
 
@@ -7,20 +9,20 @@ int WindowCreation::makeNewWindow(int argc, char* argv[])
 	SDL_GLContext _windowsOpenGlContext;
 
 	SDL_Init(SDL_INIT_VIDEO);
-
+	//Alustetaan opengl.
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	
-	
 
-	window = SDL_CreateWindow(PROGRAM_NAME, SDL_WINDOWPOS_CENTERED, 
-	SDL_WINDOWPOS_CENTERED,640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-	
+
+
+	window = SDL_CreateWindow(PROGRAM_NAME, SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	//SDL_SetWindowSize(window,);
 	if (window == NULL) {
-		
+
 		printf("Could not create window: %s\n", SDL_GetError());
 		return 1;
 	}
@@ -31,15 +33,25 @@ int WindowCreation::makeNewWindow(int argc, char* argv[])
 
 
 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	SDL_GL_SwapWindow(window);
-	
-	
-	BOOLEAN running=TRUE;
+
+
+	//Looppi
+	BOOLEAN running = TRUE;
 	while (running != FALSE)
 	{
+		//Putsataan ruutu
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+		//Loopin rakenne.
 		SDL_Event _engineRunningLoop;
+		//Ikkunan tappo joko x tai esc.
+		while (SDL_PollEvent(&_engineRunningLoop))
+		{
+			if (_engineRunningLoop.type == SDL_QUIT)
+				running = false;
+		}
+
 		while (SDL_PollEvent(&_engineRunningLoop))
 		{
 			switch (_engineRunningLoop.type)
@@ -65,10 +77,13 @@ int WindowCreation::makeNewWindow(int argc, char* argv[])
 				break;
 			}
 		}
+		//Loopin Tehtävät.
+
 	}
+	//Tuhotaan Ikkuna.
 	SDL_GL_DeleteContext(_windowsOpenGlContext);
 	SDL_DestroyWindow(window);
-	
+
 	SDL_Quit();
 	return 0;
 };
