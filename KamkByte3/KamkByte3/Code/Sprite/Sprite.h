eb0b00995e4366c6af94d441401ccf84b150ff34
SikSakJiiPee <juissi93@hotmail.com>
2015-12-04 12:04:24 +0200

    Sprite uuteen kansioon

---
+++ KamkByte3/KamkByte3/Code/Sprite/Sprite.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "..\..\Plugins\glew\include\GL\glew.h"
+
+
+class Sprite
+{
+public:
+	Sprite();
+	~Sprite();
+
+	void init(float posX, float posY, float width, float height);
+
+	void draw();
+
+private:
+	float _x;
+	float _y;
+	float _width;
+	float _height;
+	GLuint _vboID;
+};
+
